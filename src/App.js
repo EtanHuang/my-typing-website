7e924c6a2a61541b2bc8d395e45b81c8182e4928
Etan Huang <ehuang09@students.cs.ubc.ca>
2023-08-23 23:49:45 -0700

    Made a NavBar and LoginPage 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/NavBar.js 	new file:   src/firebase.js 	new file:   src/pages/LoginPage.js 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import './App.css';
 import Test from './pages/Test';
+import NavBar from './components/NavBar';
+import LoginPage from './pages/LoginPage';
 
 
 function App() {
+  console.log(window.location);
+  let component = null;
+  switch (window.location.pathname) {
+    case "/test":
+      component = <Test />
+      break
+    case "/login":
+      component = <LoginPage />
+      break
+  }
   return (
     <div>
-      <Test />
+      {component}
+      <NavBar/>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { Link, Routes, Route } from "react-router-dom";
+import "../App.css";
 
 function NavBar() {
     return (
         <div>
-            <li>
-                <Link to="/">Test</Link>
-            </li>
+            <nav className = "nav">
+                <a href="/test" className="title">
+                    The Typing Test
+                </a>
+            <div>
+                <li>
+                    <a href="/test">Test</a>
+                </li>
+                <li>
+                    <a href="/login">Login</a>
+                </li>
+            </div>
+            </nav>
         </div>
+        
     )
 };
 
